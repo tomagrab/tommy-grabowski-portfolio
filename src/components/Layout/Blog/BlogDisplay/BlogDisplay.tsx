f316884a223796035c6c4f0b794fc15af92eb528
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-02-22 06:56:09 -0500

    Updated BlogDisplay for non-authenticated users again

--- src/components/Layout/Blog/BlogDisplay/BlogDisplay.tsx
+++ src/components/Layout/Blog/BlogDisplay/BlogDisplay.tsx
@@ -77,7 +77,8 @@ const BlogPostHeader = ({
         <div className="flex flex-col pb-4">
           <h2 className="blog_post--header__title">{post?.title}</h2>
           <small>
-            Written by {post.author} on {FormatDate(post.createdAt)}
+            Written by <strong>{post.author}</strong> on{' '}
+            {FormatDate(post.createdAt)}
           </small>
           <small>Last updated on {FormatDate(post.updatedAt)}</small>
         </div>
