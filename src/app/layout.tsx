d11736720cd17ae1ef37de755470dbaca05d895f
Tommy Boi <tomaarongrabowski@gmail.com>
2024-02-22 19:54:19 -0500

    Added web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
-import "./Layout.scss";
-import Navbar from "@/components/Layout/Navbar/Navbar";
-import Header from "@/components/Layout/Header/Header";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { dark } from "@clerk/themes";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.scss';
+import './Layout.scss';
+import Navbar from '@/components/Layout/Navbar/Navbar';
+import Header from '@/components/Layout/Header/Header';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { dark } from '@clerk/themes';
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "TG",
+  title: 'TG',
   description:
     "Tommy Grabowski's Portfolio: Explore React projects, blog posts, and my web development journey. Learn about my skills and experience.",
   keywords: [
-    "Tommy Grabowski",
-    "React",
-    "web development",
-    "portfolio",
-    "blog",
-    "projects",
+    'Tommy Grabowski',
+    'React',
+    'web development',
+    'portfolio',
+    'blog',
+    'projects',
   ],
 };
 
@@ -44,6 +45,7 @@ export default function RootLayout({
               <div className="layout__main--outer bg-slate-200 p-4">
                 <ScrollArea className="layout__main--inner w-full rounded bg-mint-cream p-4 shadow-xl">
                   {children}
+                  <Analytics />
                 </ScrollArea>
               </div>
             </div>
