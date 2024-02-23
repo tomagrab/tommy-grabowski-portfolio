9b9a3684eba87e250585b509c0d3326e56c99be0
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-02-23 10:29:13 -0500

    Updated About loading page

--- src/app/About/loading.tsx
+++ src/app/About/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <main>
       <LoadingHeader title="About" />
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4">
         <LoadingCard
           cardClassName="w-full h-96 bg-gray-200 rounded-md p-4 flex flex-col justify-between"
           cardHeaderSkeletonClassName="h-6 w-full rounded-md py-4"
@@ -13,7 +13,7 @@ export default function Loading() {
           cardContentSkeletonClassName="h-4 w-full rounded-md"
           numberOfCardContentSkeletons={3}
         />
-        <div className="grid grid-cols-2 gap-2">
+        <div className="flex flex-wrap gap-4 md:flex-nowrap">
           <LoadingCard
             cardClassName="w-full h-96 bg-gray-200 rounded-md p-4 flex flex-col justify-between"
             cardHeaderSkeletonClassName="h-6 w-full rounded-md py-4"
